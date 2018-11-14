c01bec22c3c36b178caf8dffd1816a88a041efd8
Bill Ni <bni@stuy.edu>
2018-11-13 20:55:15 -0500

    hw24

---
+++ 24_rest/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+from urllib import request, parse, error
+import json
+
+app = Flask(__name__)
+
+@app.route("/")
+def display():
+	url = "https://api.nasa.gov/planetary/earth/imagery/?"
+	data = {}
+	data["lon"] = "100.75"
+	data["lat"] = "1.5"
+	data["date"] = "2014-02-01"
+	data["cloud_score"] = "True"
+	data["api_key"] = "dfWBHYGg1JQCpauQJ08a7QpdPiaXz9wTN4hfiXeX"
+
+	url += parse.urlencode(data)
+
+	f = request.urlopen(url).read()
+	d = json.loads(f)
+	return render_template("main.html", piclink=d["url"])
+
+app.debug = True
+app.run()
